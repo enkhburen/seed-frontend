2241c15777d0be5389667b3c25cfd6fd5b021666
Enkhburen Enkhbayar <hreverse2@gmail.com>
2022-05-03 00:11:51 +0800

    done

--- components/page/home/landing-carousel/index.tsx
+++ components/page/home/landing-carousel/index.tsx
@@ -1,10 +1,12 @@
+import React from 'react'
 import Image from 'next/image'
 import Slider from 'react-slick'
 import Box from '@mui/material/Box'
+// import CircularProgress from '@mui/material/CircularProgress'
 
-import Slider1 from '/public/assets/slider/slider1.jpg'
-import Slider2 from '/public/assets/slider/slider2.jpg'
-import Slider3 from '/public/assets/slider/slider3.jpg'
+import Slider1 from 'public/assets/slider/slider1.jpg'
+import Slider2 from 'public/assets/slider/slider2.jpg'
+import Slider3 from 'public/assets/slider/slider3.jpg'
 
 var sliderSettings = {
 	arrow: true,
@@ -16,32 +18,25 @@ var sliderSettings = {
 }
 
 export default function LandingCarousel() {
+	// const [data, setData] = React.useState(null);
+
+	// if (data == null)
+	// 	return <CircularProgress />
+	// else
+
+	// setTimeout(() => {  console.log("World!"); }, 5000);
+
 	return (
 		<Box>
 			<Slider {...sliderSettings}>
 				<div>
-					<Image
-						src={Slider1.src}
-						width="1900"
-						height="580"
-						alt="Slider 1"
-					></Image>
+					<Image src={Slider1} width="1900" height="580" alt="Slider 1"></Image>
 				</div>
 				<div>
-					<Image
-						src={Slider2.src}
-						width="1900"
-						height="580"
-						alt="Slider 2"
-					></Image>
+					<Image src={Slider2} width="1900" height="580" alt="Slider 2"></Image>
 				</div>
 				<div>
-					<Image
-						src={Slider3.src}
-						width="1900"
-						height="580"
-						alt="Slider 3"
-					></Image>
+					<Image src={Slider3} width="1900" height="580" alt="Slider 3"></Image>
 				</div>
 			</Slider>
 		</Box>

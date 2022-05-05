109d8292686af2081a229462087e6f2968343570
Enkhburen Enkhbayar <hreverse2@gmail.com>
2022-05-06 01:49:27 +0800

    api data fixed

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -14,43 +14,43 @@ import BounceLoader from 'react-spinners/BounceLoader'
 
 const clientSideEmotionCache = createEmotionCache()
 
-function Loading(): any {
-	const router = useRouter()
-	const [loading, setLoading] = useState(false)
+// function Loading(): any {
+// 	const router = useRouter()
+// 	const [loading, setLoading] = useState(false)
 
-	useEffect(() => {
-		const handleStart = (url: any) => url !== router.asPath && setLoading(true)
-		const handleComplete = (url: any) =>
-			url === router.asPath &&
-			setTimeout(() => {
-				setLoading(false)
-			}, 3000)
-		router.events.on('routeChangeStart', handleStart)
-		router.events.on('routeChangeComplete', handleComplete)
-		router.events.on('routeChangeError', handleComplete)
+// 	useEffect(() => {
+// 		const handleStart = (url: any) => url !== router.asPath && setLoading(true)
+// 		const handleComplete = (url: any) =>
+// 			url === router.asPath &&
+// 			setTimeout(() => {
+// 				setLoading(false)
+// 			}, 3000)
+// 		router.events.on('routeChangeStart', handleStart)
+// 		router.events.on('routeChangeComplete', handleComplete)
+// 		router.events.on('routeChangeError', handleComplete)
 
-		return () => {
-			router.events.off('routeChangeStart', handleStart)
-			router.events.off('routeChangeComplete', handleComplete)
-			router.events.off('routeChangeError', handleComplete)
-		}
-	})
+// 		return () => {
+// 			router.events.off('routeChangeStart', handleStart)
+// 			router.events.off('routeChangeComplete', handleComplete)
+// 			router.events.off('routeChangeError', handleComplete)
+// 		}
+// 	})
 
-	return (
-		loading && (
-			<div className="spinner-wrapper">
-				<BounceLoader color={'#127F0C'} size={100} />
-			</div>
-		)
-	)
-}
+// 	return (
+// 		loading && (
+// 			<div className="spinner-wrapper">
+// 				<BounceLoader color={'#127F0C'} size={100} />
+// 			</div>
+// 		)
+// 	)
+// }
 
 const MyApp = (props: any) => {
 	const { Component, emotionCache = clientSideEmotionCache, pageProps } = props
 
 	return (
 		<>
-			<Loading />
+			{/* <Loading /> */}
 			<CacheProvider value={emotionCache}>
 				<ThemeProvider theme={lightTheme}>
 					<CssBaseline />

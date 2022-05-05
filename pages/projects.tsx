76c3355e7e8fd8631b6dfad7c8a2213c02b75089
Enkhburen Enkhbayar <hreverse2@gmail.com>
2022-05-06 02:09:07 +0800

    runsh editted

--- pages/projects.tsx
+++ pages/projects.tsx
@@ -1,8 +1,9 @@
 import Head from 'next/head'
 
 import Box from '@mui/material/Box'
+import Container from '@mui/material/Container'
 import PageLayout from 'layout/page-layout'
-
+import Button from '@mui/material/Button'
 import ProjectsList from 'components/page/projects/projects-list'
 
 export default function ProjectsPage() {
@@ -12,6 +13,15 @@ export default function ProjectsPage() {
 				<title>Төслүүд - Seed.mn</title>
 			</Head>
 			<Box sx={{ backgroundColor: '#edf9f3' }}>
+				{/* <Container>
+					<Box sx={{ textAlign: 'center', pt: 5 }}>
+						<Button href="/projects/movies">Кино</Button>
+						<Button href="/projects/songs">Дуу</Button>
+						<Button href="/projects/clips">Клип</Button>
+						<Button href="/projects/events">Эвент</Button>
+						<Button href="/projects/other">Бусад</Button>
+					</Box>
+				</Container> */}
 				<ProjectsList />
 			</Box>
 		</PageLayout>
--- run.sh
+++ run.sh
@@ -3,6 +3,7 @@
 # THIS SCRIPT FOR DEVELOPMENT ENVIRONMENT
 
 pm2 stop seed.mn
+pm2 delete seed.mn
 
 # assuming you have a GitHub repository for the app
 git pull origin main
@@ -16,5 +17,4 @@ yarn global add pm2 # install pm2 for running our app detached
 pm2 start npm --name "seed.mn" -- start # start next app
 # pm2 stop next # for stopping app
 
-echo "MOVE BITCH GET OUT DA WAY!"
 echo "DONE"

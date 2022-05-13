d738449bdc86065e59a45ce206b97c50edfaca8b
enkhbilguutei <enkhbilguutei01@gmail.com>
2022-05-13 18:50:02 +0800

    OTP

--- components/page/verify/index.tsx
+++ components/page/verify/index.tsx
@@ -1,3 +1,4 @@
+import React from 'react'
 import { Formik } from 'formik'
 import * as yup from 'yup'
 import useScriptRef from 'hooks/useScriptRef'
@@ -16,11 +17,44 @@ import {
 	InputLabel,
 	OutlinedInput,
 	Stack,
+	TextField,
 	Typography,
 	useMediaQuery
 } from '@mui/material'
 
 export default function Verify() {
+	const [inputRefs, setInputRefs] = React.useState<any[]>([])
+	const [values, setValues] = React.useState<string[]>([])
+	const inputLength: number = 6
+
+	const handleChange = (event: any, index: number) => {
+		event.preventDefault()
+
+		if (event.target.value.length > 1) return
+
+		let updatedValue = [...values]
+		updatedValue[index] = event.target.value
+
+		setValues(updatedValue)
+
+		if (inputLength > index + 1 && event.target.value !== '')
+			inputRefs[index + 1].current.focus()
+	}
+
+	const handleVerify = () => {
+		const value: string = values.join('')
+	}
+
+	React.useEffect(() => {
+		setInputRefs((inputRef) =>
+			Array(inputLength)
+				.fill(null)
+				.map((_, i) => inputRef[i] || React.createRef())
+		)
+
+		setValues((value) => Array(inputLength).fill(''))
+	}, [])
+
 	return (
 		<Box
 			width="40%"
@@ -50,6 +84,37 @@ export default function Verify() {
 				example@gmail.com руу илгээсэн 6 оронтой баталгаажуулах кодыг оруулна
 				уу.
 			</Typography>
+			<FormControl
+				sx={{ widht: '300px', display: 'block', textAlign: 'center', py: 2 }}
+			>
+				{values.map((value: string, index: number) => (
+					<OutlinedInput
+						key={index}
+						placeholder="_"
+						value={value}
+						onChange={(event) => handleChange(event, index)}
+						inputRef={inputRefs[index]}
+						sx={{
+							maxHeight: '48px',
+							maxWidth: '48px',
+							display: 'inline-block',
+							mr: 1,
+							textAlign: 'center',
+							overflow: 'hidden'
+						}}
+					/>
+				))}
+			</FormControl>
+			<Button
+				variant="contained"
+				fullWidth
+				type="submit"
+				size="small"
+				sx={{ backgroundColor: '#127f06' }}
+				onClick={(e) => handleVerify()}
+			>
+				Баталгаажуулах
+			</Button>
 		</Box>
 	)
 }

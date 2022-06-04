98d3557f0759249029dae943b4e75a716d42a3c3
Enkhburen Enkhbayar <hreverse2@gmail.com>
2022-06-04 21:40:09 +0800

    push

--- components/common/menu/index.tsx
+++ components/common/menu/index.tsx
@@ -1,7 +1,9 @@
-import React, { useState } from 'react'
+import * as React from 'react'
+import { useState } from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 import { useRouter } from 'next/router'
+import Cookies from 'universal-cookie'
 
 import AppBar from '@mui/material/AppBar'
 import Box from '@mui/material/Box'
@@ -9,6 +11,10 @@ import Container from '@mui/material/Container'
 import Typography from '@mui/material/Typography'
 import Grid from '@mui/material/Grid'
 import Button from '@mui/material/Button'
+import Avatar from '@mui/material/Avatar'
+import Tooltip from '@mui/material/Tooltip'
+import IconButton from '@mui/material/IconButton'
+import MenuItem from '@mui/material/MenuItem'
 
 import MenuButton from './components/MenuButton'
 
@@ -37,36 +43,42 @@ const menus = [
 	}
 ]
 
-export default function Menu() {
-	const [isLogged, setIsLogged] = useState(false)
-
-	const changeLoginButton = () => {
-		if (isLogged == false) {
-			return (
-				<Button
-					sx={{ dislay: 'block' }}
-					// href="/auth/login"
-					onClick={() => handleClick(true)}
-					color="primary"
-					variant="outlined"
-				>
-					Нэвтрэх
-				</Button>
-			)
-		} else {
-			return (
-				<Button
-					sx={{ dislay: 'block' }}
-					// href="/auth/login"
-					onClick={() => handleClick(false)}
-					color="primary"
-					variant="outlined"
-				>
-					Гарах
-				</Button>
-			)
-		}
+const settings = [
+	{
+		title: 'Профайл',
+		url: '/user/'
+	},
+	{
+		title: 'Тохиргоо',
+		url: '/settings'
+	},
+	{
+		title: 'Гарах',
+		url: '/auth/logout'
 	}
+]
+
+export default function Menu() {
+	const [anchorElUser, setAnchorElUser] = useState(null)
+	const handleOpenUserMenu = (event: React.MouseEvent<HTMLElement>) => {
+		setAnchorElUser(event.currentTarget)
+	}
+
+	const handleCloseUserMenu = () => {
+		setAnchorElUser(null)
+	}
+
+	const cookies = new Cookies()
+
+	cookies.set(
+		'token',
+		'eyJraWQiOiJ5UEd5cUpoMVg5WklnNnpkY25PZWN5eFBhbFArQ0prb2RJaTBMYmkraGFFPSIsImFsZyI6IlJTMjU2In0.eyJzdWIiOiJiNDI4NmY4YS1lMzliLTRmMTgtODQ1Yy0yNzdlZGI4M2MzNzAiLCJpc3MiOiJodHRwczpcL1wvY29nbml0by1pZHAuYXAtc291dGhlYXN0LTEuYW1hem9uYXdzLmNvbVwvYXAtc291dGhlYXN0LTFfQ1ZJMFlKa2piIiwidmVyc2lvbiI6MiwiY2xpZW50X2lkIjoiMWszZGhlbTViaDBhNzNkbmZsbXRvcG1raTYiLCJldmVudF9pZCI6IjU5NTk3YTA3LTQwYmQtNDg1Yy1iNWQ1LWNiNmRlMmNmODZkZCIsInRva2VuX3VzZSI6ImFjY2VzcyIsInNjb3BlIjoiYXdzLmNvZ25pdG8uc2lnbmluLnVzZXIuYWRtaW4gcGhvbmUgb3BlbmlkIHByb2ZpbGUgZW1haWwiLCJhdXRoX3RpbWUiOjE2NTM2MzQ1NzEsImV4cCI6MTY1MzYzODE3MSwiaWF0IjoxNjUzNjM0NTcxLCJqdGkiOiI5ZTY1YzY2Ni01OTgwLTQ4ZDEtYWQ0NC1iYmJkNTI3YmYyZTUiLCJ1c2VybmFtZSI6ImI0Mjg2ZjhhLWUzOWItNGYxOC04NDVjLTI3N2VkYjgzYzM3MCJ9.JEoaFbIRtj3LHnO1OALKYrQi7y0RGldmv6DWwB5vCXytwmXHRT4_b9vQbeuXU79SPY1MeVnC8DUnNktg2TChP2-t7FHJRVP2H78cNoeKXx-nh3oXky58hkUwHgQju6NIrStZ_0_X6S7yTeADWWLNLlgEgybhvrEKKNAg0Q0S1_sZWqi4xUBXRBXGUsDNkrLYDaquUqraYMJRwI2-_sxanrsv4f5GCW9zGJIfIWdfoGi4L7QUGtGJGTZa_T_i2EDPV_g6BEQo-mZy0Pe4vmUdu41nfzXPXqOdcFDQ9SviyFYX6i47pT_mMgnLLHuzHhzceRXwFrr9xZwPIxgn-yrbVw',
+		{ path: '/' }
+	)
+
+	const [isLogged, setIsLogged] = useState(
+		cookies.get('token') !== '' ? true : false
+	)
 
 	const handleClick = (value: any) => {
 		setIsLogged(value)
@@ -130,7 +142,35 @@ export default function Menu() {
 					</Grid>
 
 					<Grid item xs={3} sx={{ textAlign: 'right' }}>
-						{changeLoginButton()}
+						<Box sx={{ flexGrow: 0 }}>
+							<Tooltip title="Open settings">
+								<IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
+									<Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+								</IconButton>
+							</Tooltip>
+							<Menu
+								sx={{ mt: '45px' }}
+								id="menu-appbar"
+								anchorEl={anchorElUser}
+								anchorOrigin={{
+									vertical: 'top',
+									horizontal: 'right'
+								}}
+								keepMounted
+								transformOrigin={{
+									vertical: 'top',
+									horizontal: 'right'
+								}}
+								open={Boolean(anchorElUser)}
+								onClose={handleCloseUserMenu}
+							>
+								{settings.map((setting, index) => (
+									<MenuItem key={index} onClick={handleCloseUserMenu}>
+										<Typography textAlign="center">{setting.title}</Typography>
+									</MenuItem>
+								))}
+							</Menu>
+						</Box>
 					</Grid>
 				</Grid>
 			</Container>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
-import { CacheProvider } from '@emotion/react'
-import { ThemeProvider, CssBaseline } from '@mui/material'
 import { useState, useEffect } from 'react'
 import { useRouter } from 'next/router'
+import { CacheProvider } from '@emotion/react'
+import { ThemeProvider, CssBaseline } from '@mui/material'
 
 import createEmotionCache from '../utils/createEmotionCache'
 import lightTheme from '../styles/theme/lightTheme'
@@ -14,43 +14,43 @@ import BounceLoader from 'react-spinners/BounceLoader'
 
 const clientSideEmotionCache = createEmotionCache()
 
-// function Loading(): any {
-// 	const router = useRouter()
-// 	const [loading, setLoading] = useState(false)
+function Loading(): any {
+	const router = useRouter()
+	const [loading, setLoading] = useState(false)
 
-// 	useEffect(() => {
-// 		const handleStart = (url: any) => url !== router.asPath && setLoading(true)
-// 		const handleComplete = (url: any) =>
-// 			url === router.asPath &&
-// 			setTimeout(() => {
-// 				setLoading(false)
-// 			}, 3000)
-// 		router.events.on('routeChangeStart', handleStart)
-// 		router.events.on('routeChangeComplete', handleComplete)
-// 		router.events.on('routeChangeError', handleComplete)
+	useEffect(() => {
+		const handleStart = (url: any) => url !== router.asPath && setLoading(true)
+		const handleComplete = (url: any) =>
+			url === router.asPath &&
+			setTimeout(() => {
+				setLoading(false)
+			}, 3000)
+		router.events.on('routeChangeStart', handleStart)
+		router.events.on('routeChangeComplete', handleComplete)
+		router.events.on('routeChangeError', handleComplete)
 
-// 		return () => {
-// 			router.events.off('routeChangeStart', handleStart)
-// 			router.events.off('routeChangeComplete', handleComplete)
-// 			router.events.off('routeChangeError', handleComplete)
-// 		}
-// 	})
+		return () => {
+			router.events.off('routeChangeStart', handleStart)
+			router.events.off('routeChangeComplete', handleComplete)
+			router.events.off('routeChangeError', handleComplete)
+		}
+	})
 
-// 	return (
-// 		loading && (
-// 			<div className="spinner-wrapper">
-// 				<BounceLoader color={'#127F0C'} size={100} />
-// 			</div>
-// 		)
-// 	)
-// }
+	return (
+		loading && (
+			<div className="spinner-wrapper">
+				<BounceLoader color={'#127F0C'} size={100} />
+			</div>
+		)
+	)
+}
 
 const MyApp = (props: any) => {
 	const { Component, emotionCache = clientSideEmotionCache, pageProps } = props
 
 	return (
 		<>
-			{/* <Loading /> */}
+			<Loading />
 			<CacheProvider value={emotionCache}>
 				<ThemeProvider theme={lightTheme}>
 					<CssBaseline />
